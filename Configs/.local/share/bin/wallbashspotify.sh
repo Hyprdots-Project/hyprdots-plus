27d1c6c67b98ddd3756f2a15b7a5bcaaa98420c2
amit-0i <6329amit.cs@gmail.com>
2025-09-14 12:15:08 +0530

    refactor: update spotify theme and change package

--- Configs/.local/share/bin/wallbashspotify.sh
+++ Configs/.local/share/bin/wallbashspotify.sh
@@ -9,23 +9,26 @@ source "${scrDir}/globalcontrol.sh"
 
 # regen conf
 
-if pkg_installed spotify && pkg_installed spicetify-cli ; then
+if pkg_installed spotify-launcher && pkg_installed spicetify-cli ; then
 
-    if [ ! -w /opt/spotify ] || [ ! -w /opt/spotify/Apps ]; then
-        notify-send -a "Hyprdots" "Permission needed for Wallbash Spotify theme"
-        pkexec chmod a+wr /opt/spotify
-        pkexec chmod a+wr /opt/spotify/Apps -R
-    fi
+    # This was needed for spotify package from AUR.
+    # Commenting it out for people who still wanna use the AUR version
+
+    #if [ ! -w /opt/spotify ] || [ ! -w /opt/spotify/Apps ]; then
+    #    notify-send -a "Hyprdots" "Permission needed for Wallbash Spotify theme"
+    #    pkexec chmod a+wr /opt/spotify
+    #    pkexec chmod a+wr /opt/spotify/Apps -R
+    #fi
 
     if [ "$(spicetify config | awk '{if ($1=="color_scheme") print $2}')" != "Wallbash" ] || [[ "${*}" == *"--reset"*  ]] ; then
         spicetify &> /dev/null
         mkdir -p ~/.config/spotify
-        touch ~/.config/spotify/prefs
+        ouch ~/.config/spotify/prefs
         sptfyConf=$(spicetify -c)
         spotfy_flags='--ozone-platform=wayland'
         sed -i -e "/^prefs_path/ s+=.*$+= $HOME/.config/spotify/prefs+g" \
             -e "/^spotify_launch_flags/ s+=.*$+= $spotfy_flags+g" "$sptfyConf"
-	    curl -L -o ${cacheDir}/landing/Spotify_Sleek.tar.gz https://github.com/prasanthrangan/hyprdots/raw/main/Source/arcs/Spotify_Sleek.tar.gz
+	    curl -L -o ${cacheDir}/landing/Spotify_Sleek.tar.gz https://github.com/Hyprdots-Project/hyprdots-plus/blob/main/Source/arcs/Spotify_Sleek.tar.gz
         tar -xzf ${cacheDir}/landing/Spotify_Sleek.tar.gz -C ~/.config/spicetify/Themes/
         spicetify backup apply
         spicetify config current_theme Sleek
